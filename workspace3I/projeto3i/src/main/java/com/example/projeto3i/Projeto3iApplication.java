35f79e104274954c7a6de0f1806c4276f52bb0ef
SarahIngrid <135475008+SarahIngrid@users.noreply.github.com>
2023-06-19 10:00:25 -0300

    workspace3I

---
+++ workspace3I/projeto3i/src/main/java/com/example/projeto3i/Projeto3iApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto3i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto3iApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto3iApplication.class, args);
+	}
+
+}
---
+++ workspace3I/projeto3i/src/test/java/com/example/projeto3i/Projeto3iApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto3i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto3iApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
